6b4a8289ef21b67881ce5edc75b872a0dc8f44c9
JaySebagh <jaysebagh@gmail.com>
2019-03-22 08:43:13 -0700

    added READ GET for ID

--- data/project/project-router.js
+++ data/project/project-router.js
@@ -16,4 +16,19 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const projectId = req.params.id;
+    projectdb.getProjectActions(projectId)
+        .then(project => {
+            if (project) {
+                res.status(200).json(project);
+            } else {
+                res.status(404).json({ message: "The project with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The project information could not be retrieved." });
+        });
+})
+
 module.exports = router;
